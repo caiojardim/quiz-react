642e579950e20c8c4bf9f42df5d38fda5f54a80d
Caio Jardim <caiojardinho@gmail.com>
2021-06-13 12:10:13 -0300

    Created component Quizes, created cards and make it responsive whith css grid

---
+++ src/components/templates/Quizes.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Quizes.css";
+
+function Quizes(props) {
+	return (
+		<div className="quizes-container">
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+			<a href="#" className="quizes-card">
+				<img
+					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
+					alt="Imagem Naruto"
+				/>
+				<span></span>
+			</a>
+		</div>
+	);
+}
+
+export default Quizes;
