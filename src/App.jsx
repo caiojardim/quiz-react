391da18bf22dda7f1a5b25d7b665a31ba112bade
Caio Jardim <caiojardinho@gmail.com>
2021-06-17 11:56:57 -0300

    added and implemented react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="container">
-			<Home />
-		</div>
+		<Router>
+			<div className="container">
+				<Switch>
+					<Route path="/quiz">
+						<Quiz />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
--- src/components/templates/Quizes.jsx
+++ src/components/templates/Quizes.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Quizes.css";
+import { Link } from "react-router-dom";
 
 function Quizes(props) {
 	return (
 		<div className="quizes-container">
-			<a href="#" className="quizes-card">
+			<Link to="/quiz" className="quizes-card">
 				<img
 					src="https://criticalhits.com.br/wp-content/uploads/2019/01/naruto-uzumaki_qabz.png"
 					alt="Imagem Naruto"
 				/>
 				<div>Você realmente conhece o Naruto? </div>
-			</a>
+			</Link>
 		</div>
 	);
 }
