ae069924ab152571cc8eba95057acbbc849d4d02
Caio Jardim <caiojardinho@gmail.com>
2021-06-19 18:42:57 -0300

    create a fake data base to make sistem dinamic

--- src/components/templates/Quizes.jsx
+++ src/components/templates/Quizes.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "./Quizes.css";
 import { Link } from "react-router-dom";
+import { quizes } from "../../data/db";
 
 function Quizes(props) {
 	return (
 		<div className="quizes-container">
-			<Link to="/quiz/1" className="quizes-card">
-				{/* imgs tamanho: 1440 x 1080 */}
-				<img src="assets/quizes-imgs/naruto.png" alt="Imagem Naruto" />
-				<div>Você realmente conhece o Naruto? </div>
-			</Link>
-			<Link to="/quiz/2" className="quizes-card">
-				<img src="assets/quizes-imgs/goku.jpg" alt="Imagem Goku" />
-				<div>Você realmente conhece o Goku? </div>
-			</Link>
-			<Link to="/quiz/3" className="quizes-card">
-				<img src="assets/quizes-imgs/friends.jpg" alt="Imagem Friends" />
-				<div>Você realmente conhece o Friends? </div>
-			</Link>
+			{quizes.map((quiz) => {
+				return (
+					<Link to={`/quiz/${quiz.id}`} className="quizes-card">
+						{/* imgs tamanho: 1440 x 1080 */}
+						<img src={quiz.img} alt={quiz.title} />
+						<div>{quiz.title}</div>
+					</Link>
+				);
+			})}
 		</div>
 	);
 }
---
+++ src/data/db.js
@@ -0,0 +1,19 @@
+export const quizes = [
+	{
+		id: "1",
+		title: "Você realmente conhece o Naruto?",
+		img: "assets/quizes-imgs/naruto.png",
+	},
+	{
+		id: "2",
+		title: "Você realmente conhece o Goku?",
+		img: "assets/quizes-imgs/goku.jpg",
+	},
+	{
+		id: "3",
+		title: "Você realmente conhece o Friends?",
+		img: "assets/quizes-imgs/friends.jpg",
+	},
+];
+
+export const quiz = [];
