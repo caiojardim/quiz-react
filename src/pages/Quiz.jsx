ea2812e72e86b9f7faad739e1e150aae001686d9
Caio Jardim <caiojardinho@gmail.com>
2021-06-20 15:22:31 -0300

    Created the visual part of page Quiz and add some dinamic using the fake db

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import CurrentQuest from "../components/quizElements/CurrentQuest";
+import { allQuiz } from "../data/db";
+import "./Quiz.css";
 
 function Quiz(props) {
 	const params = useParams();
-	return <div>Quiz {params.id}</div>;
+	const quiz = allQuiz.find((quiz) => quiz.id === Number(params.id));
+	const { questions } = quiz;
+
+	return (
+		<div className="quiz-container">
+			<CurrentQuest quantity={8} current={3} />
+
+			{questions.map((question, i) => {
+				return (
+					<div key={i}>
+						<div className="img-container">
+							<img
+								className="question-img"
+								src={"../" + question.img}
+								alt="Img naruto"
+							/>
+						</div>
+						<div className="question-container">
+							<div>{question.title}</div>
+							{question.alternatives.map((alternative) => {
+								return (
+									<button
+										className="alt"
+										onClick={() => alert("Vrau")}
+										key={alternative.id}
+									>
+										{alternative.alt}
+									</button>
+								);
+							})}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default Quiz;
