5da5808373c54285ec4c28c83cee9438edc4fd23
Caio Jardim <caiojardinho@gmail.com>
2021-06-26 14:30:23 -0300

    changed the structure of fake backend and refactorated the aplication to use the new structure

--- src/data/db.js
+++ src/data/db.js
@@ -3,22 +3,6 @@ export const quizes = [
 		id: "1",
 		title: "Você realmente conhece o Naruto?",
 		img: "assets/quizes-imgs/naruto.png",
-	},
-	{
-		id: "2",
-		title: "Você realmente conhece o Goku?",
-		img: "assets/quizes-imgs/goku.jpg",
-	},
-	{
-		id: "3",
-		title: "Você realmente conhece o Friends?",
-		img: "assets/quizes-imgs/friends.jpg",
-	},
-];
-
-export const allQuiz = [
-	{
-		id: 1,
 		questions: [
 			{
 				title: "Quantas caldas tem a raposa de naruto?",
@@ -43,6 +27,39 @@ export const allQuiz = [
 				],
 				answers: 3,
 			},
+			{
+				title: "Por quem naruto era apaixonado quando era criança?",
+				img: "assets/quizes-imgs/naruto.png",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Hinata",
+					},
+					{
+						id: 2,
+						alt: "Sasuke",
+					},
+					{
+						id: 3,
+						alt: "Nagato",
+					},
+					{
+						id: 4,
+						alt: "Sakura",
+					},
+				],
+				answers: 4,
+			},
 		],
+	},
+	{
+		id: "2",
+		title: "Você realmente conhece o Goku?",
+		img: "assets/quizes-imgs/goku.jpg",
+	},
+	{
+		id: "3",
+		title: "Você realmente conhece o Friends?",
+		img: "assets/quizes-imgs/friends.jpg",
 	},
 ];
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import CurrentQuest from "../components/quizElements/CurrentQuest";
-import { allQuiz } from "../data/db";
+import { quizes } from "../data/db";
 import "./Quiz.css";
 
 function Quiz(props) {
 	const params = useParams();
-	const quiz = allQuiz.find((quiz) => quiz.id === Number(params.id));
+	const quiz = quizes.find((quiz) => quiz.id === params.id);
 	const { questions } = quiz;
+
+	const [currentQuestion, setCurrentQuestion] = useState(2);
+
+	function handleAlternativeButtonClick(alternativeId) {}
 
 	return (
 		<div className="quiz-container">
-			<CurrentQuest quantity={8} current={3} />
+			<CurrentQuest quantity={questions.length} current={currentQuestion} />
 
 			{questions.map((question, i) => {
-				return (
-					<div key={i}>
-						<div className="img-container">
-							<img
-								className="question-img"
-								src={"../" + question.img}
-								alt="Img naruto"
-							/>
-							<div className="question-title">{question.title}</div>
+				console.log(question.id);
+				if (i + 1 === currentQuestion) {
+					return (
+						<div key={i}>
+							<div className="img-container">
+								<img
+									className="question-img"
+									src={"../" + question.img}
+									alt="Img naruto"
+								/>
+								<div className="question-title">{question.title}</div>
+							</div>
+							<div className="question-container">
+								{question.alternatives.map((alternative) => {
+									return (
+										<button
+											className="alt"
+											onClick={() =>
+												handleAlternativeButtonClick(alternative.id)
+											}
+											key={alternative.id}
+										>
+											{alternative.alt}
+										</button>
+									);
+								})}
+							</div>
 						</div>
-						<div className="question-container">
-							{question.alternatives.map((alternative) => {
-								return (
-									<button
-										className="alt"
-										onClick={() => alert("Vrau")}
-										key={alternative.id}
-									>
-										{alternative.alt}
-									</button>
-								);
-							})}
-						</div>
-					</div>
-				);
+					);
+				}
 			})}
 		</div>
 	);
