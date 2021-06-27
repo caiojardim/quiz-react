60b73128b6766252c939100e3a009eb067deda00
Caio Jardim <caiojardinho@gmail.com>
2021-06-27 18:45:35 -0300

    added new questions for the quizes

--- src/data/db.js
+++ src/data/db.js
@@ -1,6 +1,137 @@
 export const quizes = [
 	{
 		id: "1",
+		title: "Você realmente conhece o Friends?",
+		img: "assets/quizes-imgs/friends/friends.jpg",
+		questions: [
+			{
+				id: 1,
+				title:
+					"Quem é o primeiro a descobrir sobre o relacionamento de Monica e Chandler?",
+				img: "assets/quizes-imgs/friends/friends1.jpeg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Ross",
+					},
+					{
+						id: 2,
+						alt: "Phoebe",
+					},
+					{
+						id: 3,
+						alt: "Joey",
+					},
+					{
+						id: 4,
+						alt: "Rachel",
+					},
+				],
+				answer: 3,
+			},
+			{
+				id: 2,
+				title:
+					"Com quem Rachel e Ross deixariam sua filha caso eles morressem?",
+				img: "assets/quizes-imgs/friends/friends2.jpeg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Monica",
+					},
+					{
+						id: 2,
+						alt: "Os pais de Ross",
+					},
+					{
+						id: 3,
+						alt: "Phoebe",
+					},
+					{
+						id: 4,
+						alt: "Amy, a irmã de Rachel",
+					},
+				],
+				answer: 1,
+			},
+			{
+				id: 3,
+				title:
+					"De que maneira Phoebe descobre que Monica e Chandler estão juntos?",
+				img: "assets/quizes-imgs/friends/friends3.jpeg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Ela ouve uma conversa quente dos dois por telefone",
+					},
+					{
+						id: 2,
+						alt: "Rachel deixa escapar",
+					},
+					{
+						id: 3,
+						alt: "Chandler se esquece e dá um beijo em Monica na frente dela",
+					},
+					{
+						id: 4,
+						alt: "Ela os vê transando do apartamento do peladão",
+					},
+				],
+				answer: 4,
+			},
+			{
+				id: 4,
+				title:
+					"Qual o nome do clube que Ross ajudou a criar durante o ensino médio?",
+				img: "assets/quizes-imgs/friends/friends4.jpeg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Eu Odeio a Rachel",
+					},
+					{
+						id: 2,
+						alt: "Amores de Rachel Greene",
+					},
+					{
+						id: 3,
+						alt: "Chandler Bing quem?",
+					},
+					{
+						id: 4,
+						alt: "Quem quer ser irmão da Monica?",
+					},
+				],
+				answer: 1,
+			},
+			{
+				id: 5,
+				title: "Como se chama a irmã gêmea de Phoebe?",
+				img: "assets/quizes-imgs/friends/friends5.jpeg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Phyllis",
+					},
+					{
+						id: 2,
+						alt: "Ursula",
+					},
+					{
+						id: 3,
+						alt: "Hortense",
+					},
+					{
+						id: 4,
+						alt: "Blanche",
+					},
+				],
+				answer: 2,
+			},
+		],
+	},
+	{
+		id: "2",
 		title: "Você realmente conhece o Naruto?",
 		img: "assets/quizes-imgs/naruto/naruto.png",
 		questions: [
@@ -130,39 +261,130 @@ export const quizes = [
 		],
 	},
 	{
-		id: "2",
+		id: "3",
 		title: "Você realmente conhece o Goku?",
 		img: "assets/quizes-imgs/goku/goku.jpg",
 		questions: [
 			{
 				id: 1,
-				title: "Quantas caldas tem a raposa de naruto?",
-				img: "assets/quizes-imgs/naruto/naruto1.png",
+				title: "Como é o verdadeiro nome de Goku?",
+				img: "assets/quizes-imgs/goku/goku1.jpg",
 				alternatives: [
 					{
 						id: 1,
-						alt: "7",
+						alt: "Kaxacoto",
 					},
 					{
 						id: 2,
-						alt: "1",
+						alt: "Kekarotto",
 					},
 					{
 						id: 3,
-						alt: "9",
+						alt: "Kakarotto",
 					},
 					{
 						id: 4,
-						alt: "21",
+						alt: "Kugo",
 					},
 				],
 				answer: 3,
 			},
+			{
+				id: 2,
+				title: "Quais sãos os filhos de Goku ?",
+				img: "assets/quizes-imgs/goku/goku2.jpg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Trunks e Gohan",
+					},
+					{
+						id: 2,
+						alt: "Goten e Gohan",
+					},
+					{
+						id: 3,
+						alt: "Trunks e Uub",
+					},
+					{
+						id: 4,
+						alt: "Trunks e Goten",
+					},
+				],
+				answer: 2,
+			},
+			{
+				id: 3,
+				title: "Quem é o Saiyajin que tenta superar Goku ?",
+				img: "assets/quizes-imgs/goku/goku3.jpg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Vegeta",
+					},
+					{
+						id: 2,
+						alt: "Chaos",
+					},
+					{
+						id: 3,
+						alt: "Uub",
+					},
+					{
+						id: 4,
+						alt: "Freeza",
+					},
+				],
+				answer: 1,
+			},
+			{
+				id: 4,
+				title: "Qual o nome do mestre de Goku?",
+				img: "assets/quizes-imgs/goku/goku4.jpg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Jamenba",
+					},
+					{
+						id: 2,
+						alt: "Tao Pai Pai",
+					},
+					{
+						id: 3,
+						alt: "Toguro Ototo",
+					},
+					{
+						id: 4,
+						alt: "Kame",
+					},
+				],
+				answer: 4,
+			},
+			{
+				id: 5,
+				title: "Qual inimigo de Goku matou seu pai Bardock?",
+				img: "assets/quizes-imgs/goku/goku5.jpg",
+				alternatives: [
+					{
+						id: 1,
+						alt: "Cell",
+					},
+					{
+						id: 2,
+						alt: "Freeza",
+					},
+					{
+						id: 3,
+						alt: "Majin Boo",
+					},
+					{
+						id: 4,
+						alt: "Vegeta",
+					},
+				],
+				answer: 2,
+			},
 		],
-	},
-	{
-		id: "3",
-		title: "Você realmente conhece o Friends?",
-		img: "assets/quizes-imgs/friends/friends.jpg",
 	},
 ];
